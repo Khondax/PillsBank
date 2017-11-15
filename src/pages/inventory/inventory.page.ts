b800bfe5f6b76590676dbf46ffe0cb331f6af128
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2017-11-15 22:08:42 +0000

    Vista básica y métodos de control

--- src/pages/inventory/inventory.page.ts
+++ src/pages/inventory/inventory.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { NewPillPage, PillPage } from "../pages";
+
+import { Storage } from "@ionic/storage";
+
 @Component({
     templateUrl: 'inventory.page.html',
     selector: 'inventory.page.scss'
@@ -8,8 +12,40 @@ import { NavController } from 'ionic-angular';
 
 export class InventoryPage {
 
-    constructor(public navCtrl: NavController) {
+    data = [];
 
+    constructor(public nav: NavController,
+                private storage: Storage) {
+
+    }
+
+    ionViewDidLoad(){
+        this.storage.forEach((value, key, index) =>{
+            this.data[index.valueOf()] = value;
+        });
+    }
+
+    newPill(){
+        this.nav.push(NewPillPage);
+    }
+
+    goToPill($event, key){
+        this.nav.push(PillPage, key);
+    }
+
+    //DEBUG
+    addPill(){
+        this.storage.set('test', ["Para", "60", "5"]);
+    }
+    //DEBUG
+    getPill(){
+        this.storage.get('test').then((data) => {
+            console.log(data);
+        });
+    }
+    //DEBUG
+    delete(){
+        this.storage.clear();
     }
 
 }
