f0062ef5b63d0b9c4953be0745da961d3ed68b88
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2018-01-25 16:08:58 +0000

    Nuevo método para añadir pastillas

--- src/pages/pill/pill.page.ts
+++ src/pages/pill/pill.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ViewController, NavParams } from 'ionic-angular';
+import { NavController, ViewController, NavParams, AlertController, ToastController } from 'ionic-angular';
 
 import { Storage } from "@ionic/storage";
 
@@ -14,14 +14,54 @@ export class PillPage {
     constructor(public nav: NavController,
                 private view: ViewController,
                 private navParams: NavParams,
-                private storage: Storage) {
+                private storage: Storage,
+                private alertController: AlertController,
+                private toastController: ToastController) {
 
-        this.storage.get(this.navParams.data).then((data) => {
-            this.pill = data;
-        });
+        this.pill = this.navParams.data;
 
     }
 
+     addPills(){
+        let prompt = this.alertController.create({
+            title: 'Añadir pastillas',
+            inputs: [{
+                type: 'number',
+                name: 'pastillas',
+            }],
+            buttons: [
+                {
+                    text: 'Cancelar'
+                },
+                {
+                    text: 'Añadir',
+                    handler: data => {
+                        
+                        var nPills = parseInt(this.pill.numb) + parseInt(data.pastillas);
+
+                        this.storage.set(this.pill.key, {
+                            "key": this.pill.key,
+                            "name": this.pill.name,
+                            "numb": nPills,
+                            "price": this.pill.price,
+                            "dose": this.pill.dose,
+                            "duration": this.pill.duration
+                        });
+
+
+                        let toast = this.toastController.create({
+                            message: "Se ha registrado el cambio, quedan " + nPills/this.pill.dose + " dias hasta que termine el suministro",
+                            duration: 4000,
+                            position: 'bottom'
+                        });
+
+                        toast.present();
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
 
     goBack(){
         this.view.dismiss();
