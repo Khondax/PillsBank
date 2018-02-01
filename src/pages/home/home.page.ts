e929688539d818b215d069cd25a850ed55138140
Miguel Gonzalez Villa <miguel.gonzalez.villa@gmail.com>
2018-02-01 17:25:53 +0000

    Métodos para mostrar las alarmas creadas y acceder a los elementos

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 
+import { PillPage } from "../pages";
+
 import { LocalNotifications } from '@ionic-native/local-notifications';
+import { Storage } from "@ionic/storage";
 
 @Component({
     templateUrl: 'home.page.html',
@@ -10,13 +13,16 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 
 export class HomePage {
 
+    pills: any[];
+
     constructor(public nav: NavController,
                 private platform: Platform,
-                private localNotifications: LocalNotifications) {
+                private localNotifications: LocalNotifications,
+                private storage: Storage) {
 
     }
 
-    ionViewDidLoad(){
+    ionViewDidEnter(){
         this.platform.ready().then(() => {
             this.localNotifications.hasPermission().then(function(granted){
                 if(!granted){
@@ -24,6 +30,26 @@ export class HomePage {
                 }
             });
         });
+
+        this.localNotifications.getAllScheduled().then((notifications) => {
+            this.pills = notifications;
+        });
+        
+    }
+
+    goToPill($event, alarm){
+        var pill = [];
+        this.storage.get(alarm.id).then(
+            data => {
+                pill = data
+                this.nav.push(PillPage, pill);
+            } 
+        );
+
+    }
+
+    cancelAlarm(alarm){
+
     }
 
 }
